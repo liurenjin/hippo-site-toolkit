5466676cfadff1e796078ed89901cc021cec8546
Ate Douma <a.douma@onehippo.com>
2010-11-25 14:18:45 +0000

    HREPTWO-4739: copying ecm (HST) projects to hippo-cms7

---
+++ core/src/main/java/org/hippoecm/hst/core/template/tag/LinkTag.java
@@ -0,0 +1,144 @@
+/*
+ *  Copyright 2008 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.hst.core.template.tag;
+
+import java.io.IOException;
+
+import javax.jcr.Session;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+import org.hippoecm.hst.core.HSTHttpAttributes;
+import org.hippoecm.hst.core.mapping.RelativeURLMappingImpl;
+import org.hippoecm.hst.core.mapping.URLMapping;
+import org.hippoecm.hst.core.template.node.el.ELNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Expose a node with EL stuff to the pageContext. For now only the current PageNode!
+ * 
+ *
+ */
+public class LinkTag extends SimpleTagSupport {
+    private static final Logger log = LoggerFactory.getLogger(LinkTag.class);
+
+    private String var;
+    private String location;
+    private String staticattr;
+    private ELNode item;
+
+    @Override
+    public void doTag() throws JspException, IOException {
+        PageContext pageContext = (PageContext) getJspContext();
+        HttpServletRequest request = (HttpServletRequest) pageContext.getRequest();
+        URLMapping urlMapping = (URLMapping)request.getAttribute(HSTHttpAttributes.URL_MAPPING_ATTR);
+        String href = null;
+        if(urlMapping == null) {
+            log.debug("urlMapping not set as attribute on request. Cannot rewrite a link. Try to make it relative only for staticattr");
+            if(staticattr != null ) {
+                href =  RelativeURLMappingImpl.computeRelativeUrl(staticattr, request.getRequestURI());
+            }
+        } else {
+            if(item!= null) {
+                href = urlMapping.rewriteLocation(item.getJcrNode());
+            } else if(location != null ) {
+            	// location must be a sitemapNodeName
+                href = urlMapping.rewriteLocation(location, (Session)request.getAttribute(HSTHttpAttributes.JCRSESSION_MAPPING_ATTR));
+            } else if(staticattr != null ) {
+                href = urlMapping.getLocation(staticattr);
+            }
+        }
+        if(href != null) {
+            Link link = new Link();
+            link.setHref(href);
+            link.setSrc(href);
+            link.setLocation(href);
+            pageContext.setAttribute(getVar(), link);
+        }
+    }
+
+    public ELNode getItem(){
+        return item;
+    }
+    
+
+    public void setItem(ELNode item){
+        this.item = item;
+    }
+
+    public String getVar() {
+        return var;
+    }
+
+    public void setVar(String var) {
+        this.var = var;
+    }
+    
+    public String getLocation() {
+        return location;
+    }
+
+    public void setStatic(String staticattr) {
+        this.staticattr = staticattr;
+    }
+    
+    public String getStatic() {
+        return staticattr;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    
+    public class Link {
+        /*
+         * three link attributes, all returning the same value. 
+         * in jsp, you can use link.href, link.src or link.location
+         */
+        private String href;
+        private String src;
+        private String location;
+       
+        public String getSrc() {
+            return src;
+        }
+
+        public void setSrc(String src) {
+            this.src = src;
+        }
+
+        public String getLocation() {
+            return location;
+        }
+
+        public void setLocation(String location) {
+            this.location = location;
+        }
+
+        public String getHref() {
+            return href;
+        }
+
+        public void setHref(String href) {
+            this.href = href;
+        }
+        
+    }
+
+}
---
+++ gettingstarted/site/src/main/webapp/jsp/modules/mainnav.jsp
@@ -0,0 +1,35 @@
+<%@ page contentType="text/html; charset=UTF-8" language="java" %>
+<%@ page isELIgnored="false" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/xml" prefix="x" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
+<%@ taglib uri="http://www.hippoecm.org/jsp/hst-template" prefix="hst-tmpl"%>
+  
+<hst-tmpl:module name="mainnav" var="navitems" className="org.hippoecm.hst.components.modules.navigation.RepositoryBasedNavigationModule" execute="false" render="true"/>
+
+
+<c:forEach var="item" items="${navitems}">
+  <div id="nav${item.name}">
+    <ul>
+      <li>${item.decodedName}</li>
+    <c:forEach var="subItem" items="${item.children}">
+      <ul class="subnav">
+        <c:set var="firstSubSubItem" value="${subItem.children[0]}"/>
+        <c:choose>
+          <c:when test="${firstSubSubItem!=null}">
+	    <hst-tmpl:link var="link" item="${firstSubSubItem}" />
+            <li><a href="${link.href}" title="${subItem.decodedName}">${subItem.decodedName}</a></li>
+          </c:when>
+          <c:otherwise>
+	    <hst-tmpl:link var="link" item="${subItem}" />
+            <li><a href="${link.href}" title="${subItem.decodedName}">${subItem.decodedName}</a></li>
+          </c:otherwise>
+        </c:choose>        	    
+      </ul>
+    </c:forEach>  
+    </ul>
+  </div>		
+</c:forEach>					
+
+
+
