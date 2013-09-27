574c95e5b3253f12157eccffd1f4fea3a050baa9
Ard Schrijvers <a.schrijvers@onehippo.com>
2013-09-27 11:12:50 +0000

    :HSTTWO-2702 backport of course clear nonDefaultHstQueryManagers instead of the object bean managers

--- components/core/src/main/java/org/hippoecm/hst/site/request/HstRequestContextImpl.java
+++ components/core/src/main/java/org/hippoecm/hst/site/request/HstRequestContextImpl.java
@@ -724,7 +724,7 @@ public class HstRequestContextImpl implements HstMutableRequestContext {
             nonDefaultObjectBeanManagers.clear();
         }
         if (nonDefaultHstQueryManagers != null) {
-            nonDefaultObjectBeanManagers.clear();
+            nonDefaultHstQueryManagers.clear();
         }
     }
 
