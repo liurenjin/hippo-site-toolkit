ebd02b89fce27e297b90a802ecd1b5f30872d686
Ard Schrijvers <a.schrijvers@onehippo.com>
2013-11-08 12:45:56 +0000

    HSTTWO-2728 include a token to avoid a wrong request ( for example for an image, css, etc) getting the redirect

--- commons/src/main/java/org/hippoecm/hst/container/HstFilter.java
+++ commons/src/main/java/org/hippoecm/hst/container/HstFilter.java
@@ -121,8 +121,6 @@ public class HstFilter implements Filter {
     private String [] prefixExclusions;
     private String [] suffixExclusions;
 
-    private String defaultLoginResourcePath;
-    
     public void init(FilterConfig filterConfig) throws ServletException {
         this.filterConfig = filterConfig;
 
@@ -138,8 +136,6 @@ public class HstFilter implements Filter {
 
         clientComponentManagerContextAttributeName = getConfigOrContextInitParameter(CLIENT_COMPONENT_MANAGER_CONTEXT_ATTRIBUTE_NAME_INIT_PARAM, clientComponentManagerContextAttributeName);
         
-        defaultLoginResourcePath = getInitParameter(filterConfig, null, "loginResource", DEFAULT_LOGIN_RESOURCE_PATH);
-
         prefixExclusions = splitParamValue(getConfigOrContextInitParameter(PREFIX_EXCLUSIONS_INIT_PARAM, null), ",");
         suffixExclusions = splitParamValue(getConfigOrContextInitParameter(SUFFIX_EXCLUSIONS_INIT_PARAM, null), ",");
 
@@ -315,16 +311,22 @@ public class HstFilter implements Filter {
              * Another problem occurs when the login is over https: The container normally runs over http, and hence
              * always a http redirect is done by the container. Hence we check below whether there is an attribute
              * on the http session that is only present after a jaas login attempt. If present, we do another redirect.
-             */ 
+             */
             if (doClientRedirectAfterJaasLoginBehindProxy) {
                 HttpSession session = req.getSession(false);
-                if (session != null && session.getAttribute(ContainerConstants.HST_JAAS_LOGIN_ATTEMPT_RESOURCE_URL_ATTR) != null) {
-                    String resourceURL = (String)session.getAttribute(ContainerConstants.HST_JAAS_LOGIN_ATTEMPT_RESOURCE_URL_ATTR);
-                    session.removeAttribute(ContainerConstants.HST_JAAS_LOGIN_ATTEMPT_RESOURCE_URL_ATTR);
-                    res.sendRedirect(resourceURL);
-                    return;
+                if (session != null && session.getAttribute(ContainerConstants.HST_JAAS_LOGIN_ATTEMPT_RESOURCE_TOKEN) != null ) {
+                    if (session.getAttribute(ContainerConstants.HST_JAAS_LOGIN_ATTEMPT_RESOURCE_TOKEN).equals(req.getParameter("token"))) {
+                        // we are dealing with client side redirect from the container after JAAS login. This redirect typically
+                        // fails in case of proxy taking care of the context path in front of the application.
+                        // hence we need another redirect.
+                        String resourceURL = (String)session.getAttribute(ContainerConstants.HST_JAAS_LOGIN_ATTEMPT_RESOURCE_URL_ATTR);
+                        session.removeAttribute(ContainerConstants.HST_JAAS_LOGIN_ATTEMPT_RESOURCE_URL_ATTR);
+                        session.removeAttribute(ContainerConstants.HST_JAAS_LOGIN_ATTEMPT_RESOURCE_TOKEN);
+                        res.sendRedirect(resourceURL);
+                        return;
+                    }
                 }
-    	    }
+            }
 
             request.setAttribute(ContainerConstants.VIRTUALHOSTS_REQUEST_ATTR, resolvedVirtualHost);
 
